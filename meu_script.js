9e259886e370a91dab99424968e1c508edfe8758
Gabriel Marinho <biel32771@gmail.com>
2020-05-01 16:32:59 -0300

    Primeiros arquivos javaScript

---
+++ meu_script.js
@@ -0,0 +1,16 @@
+//alert('Olá, seja bem vindo ao curso!!!')
+//Troca o valor do input
+//document.getElementById('nome').value = 'Oi'
+
+/* Variaveis */
+var num = -7
+var numFracionado = 112.11
+var teste = false 
+var calc = numFracionado + num
+alert(calc)
+
+//Escreve um texto na pagina 
+document.write(texto)
+
+// Escrece a variavel no log
+console.log(num)
